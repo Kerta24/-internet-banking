e9667952213a7ed400dbca67d4b8076dea555151
Kerta24 <kertahendrawan@gmail.com>
2023-01-19 08:14:26 +0800

    commit push history pembuatan APK

--- lib/page/loginregister/register.dart
+++ lib/page/loginregister/register.dart
@@ -27,118 +27,120 @@ class _registerState extends State<register> {
           backgroundColor: Color.fromARGB(255, 10, 1, 134),
           title: const Text("Register"),
         ),
-        body: Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            children: [
-              TextField(
-                controller: _usernameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Username',
-                ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: _namaController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Nama',
-                ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: _nimController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Nim',
-                ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: _passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-              SizedBox(height: 10),
-              TextField(
-                controller: _passwordConfirmController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Comfirm Password',
-                ),
-              ),
-              SizedBox(height: 30),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                primary: Color.fromARGB(255, 10, 1, 134),
-                onPrimary: Colors.white,
-              ),
-                onPressed: () async {
-                  // register menggunakan service
-                  if (_passwordController.text ==
-                      _passwordConfirmController.text) {
-                    await _services
-                        .registerUser(
-                            username: _usernameController.text,
-                            nama: _namaController.text,
-                            password: _passwordController.text,
-                            nim: _nimController.text)
-                        .then((value) {
-                      if (value != null && value) {
-                        print(value);
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Berhasil Daftar'),
-                          ),
-                        );
-                        Navigator.pushNamedAndRemoveUntil(
-                            context, '/login', (route) => false);
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(
-                            content: Text('Gagal Daftar'),
-                          ),
-                        );
-                      }
-                    });
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Password Tidak Sama'),
-                      ),
-                    );
-                  }
-                },
-                child: const SizedBox(
-                  width: 100,
-                  child: Text(
-                    'Daftar',
-                    textAlign: TextAlign.center,
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Username',
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              Text("Sudah mempunyai akun?"),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  TextButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => LoginScreen()));
-                      },
-                      child: Text(
-                        "Login",
-                      )),
-                  TextButton(onPressed: (null), child: Text("Lupa Pasword"))
-                ],
-              )
-            ],
+                SizedBox(height: 10),
+                TextField(
+                  controller: _namaController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nama',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _nimController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Nim',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: _passwordConfirmController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Comfirm Password',
+                  ),
+                ),
+                SizedBox(height: 30),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                  primary: Color.fromARGB(255, 10, 1, 134),
+                  onPrimary: Colors.white,
+                ),
+                  onPressed: () async {
+                    // register menggunakan service
+                    if (_passwordController.text ==
+                        _passwordConfirmController.text) {
+                      await _services
+                          .registerUser(
+                              username: _usernameController.text,
+                              nama: _namaController.text,
+                              password: _passwordController.text,
+                              nim: _nimController.text)
+                          .then((value) {
+                        if (value != null && value) {
+                          print(value);
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Berhasil Daftar'),
+                            ),
+                          );
+                          Navigator.pushNamedAndRemoveUntil(
+                              context, '/login', (route) => false);
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: Text('Gagal Daftar'),
+                            ),
+                          );
+                        }
+                      });
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Password Tidak Sama'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const SizedBox(
+                    width: 100,
+                    child: Text(
+                      'Daftar',
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text("Sudah mempunyai akun?"),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginScreen()));
+                        },
+                        child: Text(
+                          "Login",
+                        )),
+                    TextButton(onPressed: (null), child: Text("Lupa Pasword"))
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
